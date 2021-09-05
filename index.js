8d54ecb7c2fe431c9b39233ec9a0dd5d6c664626
Matt <aboutmattlaw@gmail.com>
2021-09-05 17:09:58 -0400

    commit tree 82369521fe910bd0eceae4c9633dab6f01f06780

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Your code here
+// Second Attempt 
+
+
+
+function saturdayFun (activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (flair = "*"){
+    return function innerFunction (str = "special"){
+        return `You are ${flair}${str}${flair}!`
+    }     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First Attempt 
+// // Your code here
+// function saturdayFun(activity = 'roller-skate'){
+//     return `This Saturday, I want to ${activity}!`
+// }
+
+// function mondayWork(activity = 'go to the office'){
+//     return `This Monday, I will ${activity}.`
+// }
+
+
+
+// function wrapAdjective (flair = "*"){
+//     return function innerFunction (str = "special"){
+//         return `You are ${flair}${str}${flair}!`
+//     } 
+    
+// }
